d2ee802e886de7c2908c9c83d894f963a879a59e
RakibulHasan2 <rakibulhasan99445@gmail.com>
2023-03-26 11:04:46 +0600

    set Avater backend done

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,15 +1,19 @@
 const {
-    login,
-    register,
-    getAllUsers,
-    setAvatar,
-    logOut,
-  } = require("../Controller/userController");
-  
-  const router = require("express").Router();
-  
-  router.post("/login", login);
-  router.post("/register", register);
+  login,
+  register,
+  getAllUsers,
+  setAvatar,
+  logOut,
+} = require("../Controller/userController");
 
-  
-  module.exports = router;
+const router = require("express").Router();
+
+router.post("/login", login);
+router.post("/register", register);
+router.get("/allUsers/:id", getAllUsers);
+router.post("/setAvatar/:id", setAvatar);
+router.get("/logout/:id", logOut);
+
+
+
+module.exports = router;
